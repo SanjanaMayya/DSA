0993b6f9fccfe7dc2b6b52dfc6c58f71ff9af3d3
SanjanaMayya <mayyasanjana2004@gmail.com>
2025-08-28 20:52:13 +0530

    Add files via upload

---
+++ swapcase.py
@@ -0,0 +1,5 @@
+# Write a program to swap the case of each character in a string.
+text = input("Enter a string: ")
+# Swap the case of each character in the string
+swapped_text = text.swapcase()
+print("String with swapped case:", swapped_text)
